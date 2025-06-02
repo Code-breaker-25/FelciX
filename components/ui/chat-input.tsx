2a61160ec0f29ee62b178643318243b077f04334
Pavan Kumar C <pavankumar25c@gmail.com>
2025-06-02 13:02:05 +0530

    update

--- components/ui/chat-input.tsx
+++ components/ui/chat-input.tsx
@@ -2,7 +2,8 @@ import * as React from "react";
 import { Textarea } from "@/components/ui/textarea";
 import { cn } from "@/lib/utils";
 
-interface ChatInputProps extends React.TextareaHTMLAttributes<HTMLTextAreaElement>{}
+// Option 1: Use a type alias instead of an interface
+type ChatInputProps = React.TextareaHTMLAttributes<HTMLTextAreaElement>;
 
 const ChatInput = React.forwardRef<HTMLTextAreaElement, ChatInputProps>(
   ({ className, ...props }, ref) => (
